130392a57a0ee92077d5f747b468fe0b29e15096
Noor <thenoorulhassan.dev@gmail.com>
2026-03-30 23:20:49 +0500

    Yjs setup

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,6 +1,31 @@
 import "./App.css";
 import Editor from "@monaco-editor/react";
+import { useRef, useMemo } from "react";
+import { MonacoBinding } from "y-monaco";
+import * as Y from "yjs";
+import { SocketIOProvider } from "y-socket.io";
 function App() {
+  const editorRef = useRef(null);
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+    const provider = new SocketIOProvider(
+      "http://localhost:3000",
+      "monaco",
+      ydoc,
+      {
+        autoConnect: true,
+      },
+    );
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness,
+    );
+  };
   return (
     <main className="h-screen w-full bg-gray-950 flex gap-4 p-4">
       <aside className="h-full w-1/4 bg-amber-100 rounded-lg"></aside>
@@ -10,6 +35,7 @@ function App() {
           defaultLanguage="javascript"
           theme="vs-dark"
           value="// write your code here"
+          onMount={handleMount}
         />
       </section>
     </main>
